bb2827c56cda4307beaf684f232240229971074d
Javi <jescolar@madisa.es>
2023-01-05 10:53:55 +0100

    feat(inventory): 📦 Creamos proyecto inventory

---
+++ projects/inventory/src/lib/inventory.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-inventory',
+  template: `
+    <p>
+      inventory works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class InventoryComponent {
+
+}
---
+++ projects/inventory/src/lib/inventory.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { InventoryComponent } from './inventory.component';
+
+
+
+@NgModule({
+  declarations: [
+    InventoryComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    InventoryComponent
+  ]
+})
+export class InventoryModule { }
